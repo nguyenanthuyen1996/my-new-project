bfa1e9051ccb2711c4e276db98f36c0af7ec0516
Macbook <macbook@MacBook-Pro-cua-Macbook.local>
2023-03-08 10:01:50 +0700

    first name

---
+++ index.js
@@ -0,0 +1,6 @@
+function showMessage(){
+    //alert('Hello World');
+    //document.write('Hello everybody');
+    //document.getElementById('example').innerHTML='Say Hello';
+    console.log('Hello World');
+}
